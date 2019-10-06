15173ab3927d54ee1c273a7b92287e0d5db5a586
Vasu Gupta <vasugupta1997@gmail.com>
2019-10-07 00:56:12 +0530

    Update Array_Rotation.c

--- Languages/C/Array_Rotation.c
+++ Languages/C/Array_Rotation.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
-
-void rotate(int A[], int n)
-{
-   int t = A[n-1], i;
-   for (i = n-1; i > 0; i--)
-      A[i] = A[i-1];
-   A[0] = t;
-}
-
-int main()
+main()
 {
     int n;
-    scanf("%d",&n);
-    int A[n],i;
+    scanf("%d",&n); //taking input for size of array
+    int A[n],i,t;
      for (i = 0; i < n; i++)
-        scanf("%d",&A[i]);
-
-    rotate(A, n);
-
-    printf("\nThe rotated array is : \n");
+        scanf("%d",&A[i]); //array elements input
+    t = A[n-1]; //temporary variable for swapping array elements
+    for (i = n-1; i > 0; i--)
+      A[i] = A[i-1];
+      A[0] = t; //values swapped
+    printf("\n The array after rotation is : \n");
     for (i = 0; i < n; i++)
         printf("%d ", A[i]);
-
-    return 0;
+  return 0;
 }
